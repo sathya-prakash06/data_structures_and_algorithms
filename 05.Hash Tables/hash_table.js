2d565f1b5d0c906bca65fef4513d2eefdb64ca71
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-02 16:37:18 +0530

    hash table added

---
+++ 05.Hash Tables/hash_table.js
@@ -0,0 +1,75 @@
+///////////////// Hash Table /////////////////////////
+
+//In computing, a hash table, also known as hash map, is a data structure that implements a set abstract data type,
+// a structure that can map keys to values.
+
+let hash = (string, max) => {
+  let hash = 0;
+  for (let i = 0; i < string.length; i++) {
+    hash += string.charCodeAt(i);
+  }
+  return hash % max;
+};
+
+let HashTable = function () {
+  let storage = [];
+  const storageLimit = 14;
+
+  this.print = function () {
+    console.log(storage);
+  };
+
+  this.add = function (key, value) {
+    var index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      storage[index] = [[key, value]];
+    } else {
+      let inserted = false;
+      for (let i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+      if (inserted === false) {
+        storage[index].push([key, value]);
+      }
+    }
+  };
+
+  this.remove = function (key) {
+    let index = hash(key, storageLimit);
+    if (storage[index].length === 1 && storage[index][0][0] === key) {
+      delete storage[index];
+    } else {
+      for (let i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          delete storage[index][i];
+        }
+      }
+    }
+  };
+
+  this.lookup = function (key) {
+    var index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          return storage[index][i][1];
+        }
+      }
+    }
+  };
+};
+
+console.log(hash("quincy", 10));
+
+let ht = new HashTable();
+ht.add("sathya", "dev");
+ht.add("kiran", "qa");
+ht.add("Tom", "actor");
+ht.add("Ram", "artist");
+console.log(ht.lookup("Ram"));
+ht.print();
