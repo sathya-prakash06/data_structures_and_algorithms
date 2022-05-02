2d1fe68a0d6059017f57c30e2057977370ac66a7
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-02 18:11:54 +0530

    Heap

---
+++ Heap/heap.js
@@ -0,0 +1,143 @@
+///////////////////////////// Heaps /////////////////////////////////
+
+// A Heap is a special Tree-based data structure in which the tree is a complete binary tree.
+
+// 1. Max-Heap: In a Max-Heap the key present at the root node must be greatest among the keys present at all of it’s children.
+// The same property must be recursively true for all sub-trees in that Binary Tree.
+
+// 2.Min-Heap: In a Min-Heap the key present at the root node must be minimum among the keys present at all of it’s children.
+// The same property must be recursively true for all sub-trees in that Binary Tree.
+
+// left child: i * 2
+// right child: i * 2 + 1
+// parent: i / 2
+
+let MinHeap = function () {
+  let heap = [null];
+
+  this.insert = function (num) {
+    heap.push(num);
+    if (heap.length > 2) {
+      let idx = heap.length - 1;
+      while (heap[idx] < heap[Math.floor(idx / 2)]) {
+        if (idx >= 1) {
+          [heap[Math.floor(idx / 2)], heap[idx]] = [
+            heap[idx],
+            heap[Math.floor(idx / 2)],
+          ];
+          if (Math.floor(idx / 2) > 1) {
+            idx = Math.floor(idx / 2);
+          } else {
+            break;
+          }
+        }
+      }
+    }
+  };
+
+  this.remove = function () {
+    let smallest = heap[1];
+    if (heap.length > 2) {
+      heap[1] = heap[heap.length - 1];
+      heap.splice(heap.length - 1);
+      if (heap.length == 3) {
+        if (heap[1] > heap[2]) {
+          [heap[1], heap[2]] = [heap[2], heap[1]];
+        }
+        return smallest;
+      }
+      let i = 1;
+      let left = 2 * i;
+      let right = 2 * i + 1;
+      while (heap[i] >= heap[left] || heap[i] >= heap[right]) {
+        if (heap[left] < heap[right]) {
+          [heap[i], heap[left]] = [heap[left], heap[i]];
+          i = 2 * i;
+        } else {
+          [heap[i], heap[right]] = [heap[right], heap[i]];
+          i = 2 * i + 1;
+        }
+        left = 2 * i;
+        right = 2 * i + 1;
+        if (heap[left] == undefined || heap[right] == undefined) {
+          break;
+        }
+      }
+    } else if (heap.length == 2) {
+      heap.splice(1, 1);
+    } else {
+      return null;
+    }
+    return smallest;
+  };
+
+  this.sort = function () {
+    let result = new Array();
+    while (heap.length > 1) {
+      result.push(this.remove());
+    }
+    return result;
+  };
+};
+
+let MaxHeap = function () {
+  let heap = [null];
+
+  this.print = () => heap;
+
+  this.insert = function (num) {
+    heap.push(num);
+    if (heap.length > 2) {
+      let idx = heap.length - 1;
+      while (heap[idx] > heap[Math.floor(idx / 2)]) {
+        if (idx >= 1) {
+          [heap[Math.floor(idx / 2)], heap[idx]] = [
+            heap[idx],
+            heap[Math.floor(idx / 2)],
+          ];
+          if (Math.floor(idx / 2) > 1) {
+            idx = Math.floor(idx / 2);
+          } else {
+            break;
+          }
+        }
+      }
+    }
+  };
+
+  this.remove = function () {
+    let smallest = heap[1];
+    if (heap.length > 2) {
+      heap[1] = heap[heap.length - 1];
+      heap.splice(heap.length - 1);
+      if (heap.length == 3) {
+        if (heap[1] < heap[2]) {
+          [heap[1], heap[2]] = [heap[2], heap[1]];
+        }
+        return smallest;
+      }
+      let i = 1;
+      let left = 2 * i;
+      let right = 2 * i + 1;
+      while (heap[i] <= heap[left] || heap[i] <= heap[right]) {
+        if (heap[left] > heap[right]) {
+          [heap[i], heap[left]] = [heap[left], heap[i]];
+          i = 2 * i;
+        } else {
+          [heap[i], heap[right]] = [heap[right], heap[i]];
+          i = 2 * i + 1;
+        }
+        left = 2 * i;
+        right = 2 * i + 1;
+        if (heap[left] == undefined || heap[right] == undefined) {
+          break;
+        }
+      }
+    } else if (heap.length == 2) {
+      heap.splice(1, 1);
+    } else {
+      return null;
+    }
+    return smallest;
+  };
+};
